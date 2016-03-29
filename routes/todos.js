f5d17c0e4743865b1bb5a3cd99cb9797af048286
Linxin <llx0002@gmail.com>
2016-03-29 02:16:27 -0400

    Update todos.js

--- routes/todos.js
+++ routes/todos.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var mongoose = require('mongoose');
-var user = require('../models/User.js');
+var User = require('../models/User.js');
 
 
 router.get('/test', function (req, res) {
