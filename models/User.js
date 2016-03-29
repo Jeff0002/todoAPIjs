f2dd5fc309091bfd808016cce16087a89e8e682e
Linxin <llx0002@gmail.com>
2016-03-29 02:15:12 -0400

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,10 +1,7 @@
 var mongoose = require('mongoose');
 
 var UserSchema = new mongoose.Schema({
-  name: String,
-  Age: Number,
-  LoyaltyPoints: Number,
-  FavoriteBarber: String
+  name: String
 });
 
 module.exports = mongoose.model('User', UserSchema);
